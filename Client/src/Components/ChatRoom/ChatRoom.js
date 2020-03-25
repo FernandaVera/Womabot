be1a6ced3e5cdedde7db31ae95dd0cb41ca77315
FernandaVera <dinosauriosnaranja@gmail.com>
2020-03-24 23:38:41 -0600

    ALELUYA YA SIRVEEE

--- Client/src/Components/ChatRoom/ChatRoom.js
+++ Client/src/Components/ChatRoom/ChatRoom.js
@@ -1,83 +1,95 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import './ChatStyle.css';
+import "./ChatStyle.css";
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 export const ChatRoom = () => {
-const Message = props => props.data.map(message => <p>{message.question}</p>);
-const conversation = [];
-const [data, setData] = useState(conversation);
+  // const Message = props => {<p>{props.message}</p>};
+  // const Message = props => {<p>{props}</p>};
+  // const [conversation,setConversation] = useState("");
+  // useEffect(() => {Message(conversation)},[conversation])
 
-// const MessageQueueRenderer = ({ messageQueue }) => (
-//   <>
-//       {
-//           messageQueue.map((message) =>
-//               < h5 >{message} </h5>
-//           )
-//       }
-//   </>
-// );
-const handleClick = () => {
+  const MessageQueueRenderer = ({ messageQueue }) => (
+    <>
+      {messageQueue.map(question => (
+        <h5>{question} </h5>
+      ))}
+    </>
+  );
+  const [conversation, setConversation] = useState([]);
+  const [userinput, setUserinput] = useState("");
+  useEffect(() => {
+   if (conversation.length > 0 && conversation[conversation.length-1] == userinput)
+   {var keyword;
+    switch (true) {
+      case userinput.toUpperCase().includes("HOLA"):
+        keyword = "HOLA";
+        break;
+      case userinput.toUpperCase().includes("CAUSA"):
+        keyword = "CAUSA";
+        break;
+      case userinput.toUpperCase().includes("SINTOMAS"):
+        keyword = "SINTOMAS";
+        break;
+      case userinput.toUpperCase().includes("PREVENCION"):
+        keyword = "PREVENCION";
+        break;
+      case userinput.toUpperCase().includes("AUTOEXPLORACION"):
+        keyword = "AUTOEXPLORACION";
+        break;
+      case userinput.toUpperCase().includes("MASTOGRAFIA"):
+        keyword = "MASTOGRAFIA";
+        break;
+      case userinput.toUpperCase().includes("TRATAMIENTO"):
+        keyword = "TRATAMIENTO";
+        break;
+      default:
+        keyword = "error";
+    }
+    axios
+      .get("http://localhost:9001/questions/find/" + keyword)
+      .then(function(response) {
+        updateConversation(response.data[0].answer);
+        console.log(response.data[0].answer);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    }
+  }, [conversation]);
 
-  const question = document.getElementById("messageinput").value.toUpperCase();
-  
-    var keyword;
-    switch(true) {
-      case question.includes('HOLA'):
-        keyword = 'HOLA'
-          break
-        case question.includes('CAUSA'):
-        keyword = 'CAUSA'
-          break
-        case question.includes('SINTOMAS'):
-        keyword = 'SINTOMAS'
-          break
-        case question.includes('PREVENCION'):
-        keyword = 'PREVENCION'
-          break
-        case question.includes('AUTOEXPLORACION'):
-        keyword = 'AUTOEXPLORACION'
-          break
-        case question.includes('MASTOGRAFIA'):
-        keyword = 'MASTOGRAFIA'
-            break
-        case question.includes('TRATAMIENTO'):
-        keyword = 'TRATAMIENTO'
-            break
-        default:
-          keyword = "error"
-    };
-    
-    axios.get("http://localhost:9001/questions/find/" + keyword)
-    .then(function(response){console.log(response.data[0].answer)}) // guardarlo en un state que me lo guarde en un div
-    .catch(function(err){console.log(err)})
-    // .then
-    // (function(response)
+  function updateConversation(message) {
+    setConversation([...conversation, message]);
+  }
 
-    // { this.setState({conversation:response.data.answer})
-    //   (response.data[0].answer)
-    
-    // }) 
-    // .catch(function(err){(err)})
-  
+  const handleClick = () => {
+    updateConversation(userinput);
   };
 
-  return ( 
-  <>
-    <body className="main-chat">
-      <div className="chat-room">
-        <div className="sendMess">
-
-      <Message data={data} />
-        <TextField id="messageinput" type="text" placeholder="Escribe tu pregunta aquí" variant="outlined" />
-          <Button variant="contained" onClick={() => handleClick()}> Enviar </Button>
-      
+  return (
+    <>
+      <body className="main-chat">
+        <div className="chat-room">
+          <div className="sendMess">
+            <MessageQueueRenderer messageQueue={conversation} />
+            <TextField
+              id="messageinput"
+              value={userinput}
+              onChange={({ target }) => setUserinput(target.value)}
+              type="text"
+              placeholder="Escribe tu pregunta aquí"
+              variant="outlined"
+            />
+            <Button variant="contained" onClick={() => handleClick()}>
+              {" "}
+              Preguntar{" "}
+            </Button>
+          </div>
         </div>
-      </div>
-    </body>
+      </body>
     </>
   );
 };
